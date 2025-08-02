a49271e3fdaa2fbbc0c183474d9ee006ed047caa
facuElComandante <mateorivas2022@gmail.com>
2025-08-02 17:17:48 -0300

    Add files via upload

--- Mesa5.py
+++ Mesa5.py
@@ -6,77 +6,18 @@ from pybricks.tools import wait, StopWatch
 
 hub = PrimeHub()
 
-Motor_de = Motor(Port.E)
-Motor_iz= Motor(Port.A)
-sensor_de = ColorSensor(Port.D)
-sensor_iz = ColorSensor(Port.C)
-sensor_fr = ColorSensor(Port.F)
+Motor_de = Motor(Port.A)
+Motor_iz= Motor(Port.B)
+# sensor_de = ColorSensor(Port.D)
+# sensor_iz = ColorSensor(Port.C)
+# sensor_fr = ColorSensor(Port.F)
 
 hub.imu.reset_heading(0)
-
-
-Color.WHITE = Color(h=0, s=0, v=100)
-Color.BLACK = Color(h=205, s=18, v=28)
-Color.GREEN = Color(h=162, s=75, v=52)
-my_colors = (Color.BLACK,Color.WHITE,Color.GREEN)
-
-sensor_de.detectable_colors(my_colors)
-
-sensor_iz.detectable_colors(my_colors)
-
-sensor_fr.detectable_colors(my_colors)
-
 while True:
     print(hub.imu.heading())
-    hub.imu.reset_heading(0)
-    while hub.imu.heading() > -90:
-      Motor_de.run(80)
-      Motor_iz.run(80)
-      
-    break
-print("ñlkjhgfv")
-
-while True:
-    print("Derecho", sensor_de.reflection())
-    print("Izquierdo", sensor_iz.reflection())
-    if sensor_de.reflection() > 90: 
-        Motor_de.dc(30)
-    else: 
-        Motor_de.dc(-30)
-    if sensor_iz.reflection() > 90:
-        Motor_iz.dc(-30)
-    else:
-        Motor_iz.dc(30) 
-        """   
-    if sensor_de.reflection() < 90:
-        Motor_de.dc(-20)  
-    if sensor_iz.reflection() < 90:
-        Motor_iz.dc(20)
-    """
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # hub.imu.reset_heading(0)
+    # while hub.imu.heading() > -90:
+    #     Motor_de.run(80)
+    #     Motor_iz.run(80)
+    # break
 
